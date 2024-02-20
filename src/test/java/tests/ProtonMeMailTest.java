637c7d2d16eeb6a805734f7e0200eb4357061298
Anton Klykotsiuk <Anton_Klykotsiuk@epam.com>
2024-02-20 19:20:12 +0200

    added ignore annotation

--- src/test/java/tests/ProtonMeMailTest.java
+++ src/test/java/tests/ProtonMeMailTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import steps.EclipsoEuSteps;
 import steps.ProtonMeSteps;
@@ -37,12 +38,14 @@ public class ProtonMeMailTest {
     }
 
     @Test(dataProvider = "incorrectLoginPasswordPairs")
+    @Ignore
     public void signInPageWithIncorrectCredsTest(String login, String password) {
         protonMeSteps.loginOnProtonMeMailPage(login, password, true);
         Assert.assertTrue(protonMeSteps.isAlertMessageDisplayed());
     }
 
     @Test
+    @Ignore
     public void signInPageWithEmptyCredsTest() {
         protonMeSteps.loginOnProtonMeMailPage("", "", false);
         int expectedNumberOfWarningMessages = 2;
