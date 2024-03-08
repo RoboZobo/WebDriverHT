06b697d270785dff22f6ff13f70764d03067d4c1
Anton Klykotsiuk <Anton_Klykotsiuk@epam.com>
2024-03-08 18:21:34 +0200

    removed commented text

--- src/test/java/tests/CommonTest.java
+++ src/test/java/tests/CommonTest.java
@@ -9,60 +9,6 @@ import steps.EclipsoEuSteps;
 import steps.ProtonMeSteps;
 
 public class CommonTest {
-//
-//    EclipsoEuSteps eclipsoEuSteps;
-//    private String protonUrl;
-//    private String protonLogin;
-//    private String protonPass;
-//    private String eclipsoUrl;
-//    private String eclipsoLogin;
-//    private String eclipsoPass;
-//
-//    public void sendingOfEmail(String sender, String pass, String reciever, String subject, String message) {
-//        ProtonMeSteps protonMeSteps = new ProtonMeSteps();
-//        protonMeSteps.openProtonMeMailLoginPage("firefox", protonUrl);
-//        protonMeSteps.loginOnProtonMeMailPage(sender, pass, true);
-//        protonMeSteps.clickOnNewMessageButton();
-//        protonMeSteps.fillEmailAddress(reciever);
-//        protonMeSteps.fillEmailSubject(subject);
-//        protonMeSteps.fillMessageBodyWithText(message);
-//        protonMeSteps.clickOnSendMessageButton();
-//        protonMeSteps.closeProtonMeMailPage();
-//    }
-//
-//    @BeforeMethod
-//    public void getConfig() {
-//        ConfigReader.getInstance().getConfiguration("proton");
-//        protonUrl = ConfigReader.getInstance().getProperty("loginUrl");
-//        protonLogin = ConfigReader.getInstance().getProperty("login");
-//        protonPass = ConfigReader.getInstance().getProperty("password");
-//        ConfigReader.getInstance().getConfiguration("eclipso");
-//        eclipsoUrl = ConfigReader.getInstance().getProperty("loginUrl");
-//        eclipsoLogin = ConfigReader.getInstance().getProperty("login");
-//        eclipsoPass = ConfigReader.getInstance().getProperty("password");
-//    }
-//
-//    @AfterMethod
-//    public void closePage() {
-//        eclipsoEuSteps.closeEclipsoEuMailPage();
-//    }
-//
-//    @Test
-//    public void checkOfSendingEmail() {
-//        eclipsoEuSteps = new EclipsoEuSteps();
-//        String message = "Hello, world!";
-//        String subject = "Hello!";
-//        sendingOfEmail(protonLogin, protonPass, eclipsoLogin, subject, message);
-//
-//        eclipsoEuSteps.openEclipsoEuMailPage("chrome", eclipsoUrl);
-//        eclipsoEuSteps.acceptAllCookies();
-//        eclipsoEuSteps.loginOnEclipsoEuMailPage(eclipsoLogin, eclipsoPass);
-//        eclipsoEuSteps.openInboxOnEclipsoEuMailPage();
-//        Assert.assertTrue(eclipsoEuSteps.isEmailUnread());
-//        eclipsoEuSteps.clickOnUreadEmail();
-//        Assert.assertTrue(eclipsoEuSteps.getSenderMail().contains(protonLogin));
-//        Assert.assertTrue(eclipsoEuSteps.getTextFromIframeMail().contains(message));
-//    }
 
     //provided SRP
     private EclipsoEuSteps eclipsoEuSteps;
